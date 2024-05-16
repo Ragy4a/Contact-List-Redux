3757b863f2096ba94b8f2dcc81b4f0484fe0deae
“Ragy4a” <ragy4a@gmail.com>
2024-05-16 10:28:52 +0300

    Added slice, thunks, extrareducers

--- src/store/actions/actionTypes.js
+++
@@ -1,9 +0,0 @@
-export const CONTACT_TYPES = {
-    CREATE_CONTACT: 'createContact',
-    ADD_NEW_CONTACT: 'addNewContact',
-    DELETE_CONTACT: 'deleteContact',
-    SELECT_CONTACT: 'selectContact',
-    UPDATE_CONTACT: 'updateContact',
-    SAVE_CONTACT: 'saveContact',
-    GET_CONTACTS: 'getContacts'
-}
---
+++ src/store/slices/contactSlice.js
@@ -0,0 +1,170 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+import api from '../../api/contacts-service';
+import { contactsState } from "../../model/initialState";
+import { CONTACT_SLICE_NAME } from "../../constants/constants";
+
+const initialState = {
+    contacts: contactsState,
+    editingContact: createEmptyContact(),
+    isFetching: false,
+    error: null,
+};
+
+export const getContacts = createAsyncThunk(
+    `/${CONTACT_SLICE_NAME}/getMovies`,
+    async (_, {rejectWithValue}) => {
+        try {
+            const response = await api.get(`/${CONTACT_SLICE_NAME}/`);
+            if(response.status >= 400){
+                throw new Error(`Error with getting contacts. Error status is ${response.status}`);
+            }
+            return response.data;
+        }
+        catch (error) {
+            return rejectWithValue(error.message);
+        }
+    }
+);
+
+export const createContact = createAsyncThunk(
+    `/${CONTACT_SLICE_NAME}/createContact`,
+    async (contact, {rejectWithValue, dispatch}) => {
+        try {
+            const response = await api.post(`/${CONTACT_SLICE_NAME}/`, contact);
+            if(response.status >= 400) {
+                throw new Error(`Can not create contact. Error status is ${response.status}.`);
+            };
+            dispatch(makeContact(response.data))
+        } catch (error) {
+            return rejectWithValue(error.message);
+        }
+    } 
+);
+
+export const deleteContact = createAsyncThunk(
+    `${CONTACT_SLICE_NAME}/deleteContact`,
+    async (contactId, {rejectWithValue, dispatch}) => {
+        try {
+            const response = await api.delete(`/${CONTACT_SLICE_NAME}/${contactId}`);
+            if(response.status >= 400) {
+                throw new Error(`Failed to delete contact. Error status is ${response.status}.`);
+            };
+            dispatch(removeContact(contactId));
+        } catch (error) {
+            return rejectWithValue(error.message);
+        }
+    }
+);
+
+export const editContact = createAsyncThunk(
+    `/${CONTACT_SLICE_NAME}/editContact`,
+    async (contactId, {rejectWithValue, dispatch, getState}) => {
+        const newContact = getState().contactList.contacts.editingContact;
+        try {
+            const response = await api.put(`/${CONTACT_SLICE_NAME}/${contactId}`, newContact);
+            if(response.status >= 400) {
+                throw new Error(`Failed to update contact. Error status is ${response.status}.`);
+            };
+            dispatch(changeContact(response.data));
+        } catch (error) {
+            return rejectWithValue(error.message);
+        }
+    }
+)
+
+const setFetching = (state) => {
+    state.isFetching = true; 
+    state.error = null;
+}
+
+const setError = (state, { payload }) => {
+    state.isFetching = false;
+    state.error = payload;
+}
+
+const contactSlice = createSlice({
+    name: CONTACT_SLICE_NAME,
+    initialState,
+    reducers: {
+
+        makeContact(state, { payload }) {
+            state.contacts.push(payload)
+        },
+
+        removeContact(state, { payload }) {
+            state.contacts = [
+                ...state.contacts.filter((contact) => contact.id !== payload)
+            ]
+        },
+
+        changeContact (state, { payload }) {
+            state.contacts = state.contacts.map((contact) => {
+                return contact.id === payload.id ? payload : contact
+            });
+        },
+
+        addNewContact(state) {
+            state.editingContact = createEmptyContact();
+        },
+        
+        selectContact(state, { payload }) {
+            state.editingContact = payload;
+        },
+    },
+    extraReducers: (builder) => {
+        builder
+            // Fulfilled
+            .addCase(getContacts.fulfilled, (state, { payload }) => {
+                state.isFetching = false;
+                state.error = null;
+                state.contacts = payload;
+            })
+            .addCase(createContact.fulfilled, (state, { payload }) => {
+                state.isFetching = false;
+                state.error = null;
+                state.contacts.push(payload);
+            })
+            .addCase(deleteContact.fulfilled, (state, { payload }) => {
+                state.isFetching = false;
+                state.error = null;
+                state.contacts = state.contacts.filter((contact) => contact.id !== payload)
+            })
+            .addCase(editContact.fulfilled, (state, { payload }) => {
+                state.isFetching = false;
+                state.error = null;
+                state.contacts = state.contacts.map((contact) => {
+                    return contact.id === payload.id ? payload : contact
+                });
+            })
+
+            // Pending
+            .addCase(getContacts.pending, setFetching)
+            .addCase(createContact.pending, setFetching)
+            .addCase(deleteContact.pending, setFetching)
+            .addCase(editContact.pending, setFetching)
+
+            // Error
+            .addCase(getContacts.rejected, setError)
+            .addCase(createContact.rejected, setError)
+            .addCase(deleteContact.rejected, setError)
+            .addCase(editContact.rejected, setError)
+    }
+})
+
+const { actions, reducer } = contactSlice;
+
+const { makeContact, removeContact, changeContact } = actions;
+
+export const { addNewContact, selectContact } = actions;
+
+function createEmptyContact () {
+    return {
+        fName: '',
+        lName: '',
+        phone: '',
+        email: '',
+    }
+};
+
+export default reducer;
