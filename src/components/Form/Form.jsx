8e8a2e5ad2ba0d5246030c1f9654262a39f32676
“Ragy4a” <ragy4a@gmail.com>
2024-05-29 17:40:44 +0300

    Added yup, formik, MUI

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,108 +1,118 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Field, Form, Formik, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import { TextField } from '@mui/material';
+
 import './Form.css';
 import { createContact, editContact, deleteContact } from '../../store/slices/contactSlice';
 
-const Form = () => {
-
+const ContactForm = () => {
   const dispatch = useDispatch();
-  const editingContact = useSelector(state => state.contactList.editingContact)
-  const [contact, setContact] = useState(editingContact);
+  const editingContact = useSelector(state => state.contactList.editingContact);
 
-  useEffect(() => {
-    setContact(editingContact);
-  }, [editingContact]);
-
-  const onInputChange = (event) => {
-    setContact({ 
-      ...contact, 
-      [event.target.name]: event.target.value 
-    });
+  const onContactDelete = () => {
+    dispatch(deleteContact(editingContact.id))
   }
 
-  const onContactDelete = (event) => {
-    event.preventDefault()
-    dispatch(deleteContact(contact.id))
-  }
+  const onClearInput = (fieldName, setFieldValue) => {
+    setFieldValue(fieldName, '');
+  };
 
-  const onClearInput = (event) => {
-    const sibling = event.target.previousSibling;
-    setContact({ 
-      ...contact, 
-      [sibling.name]: '',
-    });
-  }
-
-  const onFormSubmit = (event) => {
-    event.preventDefault();
-    if (!contact.id) {
-      setContact(editingContact);
-      dispatch(createContact(contact));
+  const onFormSubmit = (values) => {
+    if (!values.id) {
+      dispatch(createContact(values));
     } else {
-      dispatch(editContact(contact));
+      dispatch(editContact(values));
     }
   }
 
+  const schema = Yup.object({
+    email: Yup.string()
+      .email('Invalid email address')
+      .required('Email is required'),
+    phone: Yup.string()
+      .matches(/^[+]?[0-9]{10,15}$/, 'Invalid phone number')
+      .required('Phone number is required'),
+  });
+
+  const renderForm = ({ setFieldValue, initialValues, setValues, isValid }) => {
+
+    useEffect(() => {
+      setValues(initialValues);
+    }, [initialValues, setValues]);
+
+    return (
+      <Form id='contact-form'>
+        <div className="input-container">
+          <div className="wrapper-input">
+            <Field
+              as={TextField}
+              type="text" 
+              placeholder='First Name'
+              name='fName'
+            />
+            <span onClick={() => onClearInput('fName', setFieldValue)}>X</span>
+          </div>
+          <div className="wrapper-input">
+            <Field 
+              as={TextField}
+              type="text" 
+              placeholder='Last Name'
+              name='lName'
+            />
+            <span onClick={() => onClearInput('lName', setFieldValue)}>X</span>
+          </div>
+          <div className="wrapper-input">
+            <Field 
+              as={TextField}
+              type="email" 
+              name="email"  
+              placeholder='Email Address'
+            />
+            <span onClick={() => onClearInput('email', setFieldValue)}>X</span>
+          </div>
+            <ErrorMessage name='email'>
+              {(msg) => <div className='error'>{msg}</div>}
+            </ErrorMessage>
+          <div className="wrapper-input">
+            <Field 
+              as={TextField}
+              type="text" 
+              placeholder='Phone Number'
+              name='phone'
+            />
+            <span onClick={() => onClearInput('phone', setFieldValue)}>X</span>
+          </div>
+          <ErrorMessage name='phone'>
+              {(msg) => <div className='error'>{msg}</div>}
+            </ErrorMessage>
+        </div>
+        <div className="btn-container">
+          <button type='submit' disabled={!isValid}>Save</button>
+          {editingContact.id && (
+            <button type='button' onClick={onContactDelete}>Delete</button>
+          )}
+        </div>
+      </Form>
+    )
+  }
+
   return (
-    <>
-        <form onSubmit={onFormSubmit}>
-            <div className="input-container">
-              <div className="wrapper-input">
-                <input 
-                type="text" 
-                placeholder='First Name'
-                name='fName'
-                value={contact.fName}
-                onChange={onInputChange}
-                />
-                <span
-                onClick={onClearInput}
-                >X</span>
-                </div>
-              <div className="wrapper-input">
-                <input 
-                type="text" 
-                placeholder='Last Name'
-                name='lName'
-                value={contact.lName}
-                onChange={onInputChange}/>
-                <span
-                onClick={onClearInput}
-                >X</span>
-              </div>
-              <div className="wrapper-input">
-                <input 
-                type="email" 
-                name="email" 
-                id="email" 
-                placeholder='Email Address'
-                value={contact.email}
-                onChange={onInputChange}/>
-                <span
-                onClick={onClearInput}
-                >X</span>
-              </div>
-              <div className="wrapper-input">
-                <input 
-                type="text" 
-                placeholder='Phone Number'
-                name='phone'
-                value={contact.phone}
-                onChange={onInputChange}/>
-                <span
-                onClick={onClearInput}
-                >X</span>
-              </div>
-            </div>
-            <div className="btn-container">
-                <button type='submit'>Save</button>
-                {contact.id && (
-                  <button onClick={onContactDelete}>Delete</button>
-                )}
-            </div>
-        </form>
-    </>
+    <Formik
+      enableReinitialize
+      initialValues={{
+        fName: editingContact.fName || '',
+        lName: editingContact.lName || '',
+        email: editingContact.email || '',
+        phone: editingContact.phone || '',
+      }}
+      onSubmit={onFormSubmit}
+      validationSchema={schema}
+    >
+      {renderForm}
+    </Formik>
   );
 }
 
-export default Form;
+export default ContactForm;
